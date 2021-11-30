013d6b1eeebf5d6fd3c11f183e5743a2ad17f54a
yosefus <yosalsoni@gmail.com>
2021-11-30 09:43:00 +0200

    new upload

--- pages/mySkills.js
+++ pages/mySkills.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+
+import { FaLaptopCode } from 'react-icons/fa';
+import { BiPaint } from 'react-icons/bi';
+import { MdPhotoCamera } from 'react-icons/md';
+import { LanguageContext } from './_app';
 
 const Container = styled.div`
   width: 100vw;
@@ -11,7 +16,7 @@ const Container = styled.div`
   align-items: center;
   flex-wrap: wrap;
 
-  @media (max-height: 700px) {
+  @media (max-width: 1050px) {
     padding-top: 10vh;
   }
 `;
@@ -21,25 +26,152 @@ const Main = styled.div`
   color: ${(props) => props.theme.text};
   background: transparent;
   padding: 2rem;
-  width: 30vw;
-  height: 60vh;
+  width: 35vw;
+  min-height: 60vh;
   z-index: 3;
   line-height: 1.5;
-  font-family: 'Ubuntu Mono', monospace;
+  font-family: ${(props) => (props.lang == 'he' ? 'M PLUS 1p' : 'Ubuntu Mono')}, monospace;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-  @media (max-height: 700px) {
+  @media (max-width: 1050px) {
     margin: 5vh 0;
-    width: 60vw;
+    width: 70vw;
+  }
+  @media (max-width: 480px) {
+    padding: 2rem 1.3rem;
+  }
+  @media (max-width: 320px) {
+    width: 65vw;
   }
 `;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1rem + 1vw);
+`;
+
+const Descrition = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.4rem + 1vw);
+  padding: 0.5rem 0;
+
+  @media (max-width: 480px) {
+    font-size: calc(0.6rem + 1vw);
+  }
+`;
+
+const Text = {
+  en: {
+    designTitle: 'Web designer, photographer and graphic artist.',
+    designP:
+      'I love  to design websites in an elegant and eye-catching way. Enjoys taking and editing photos of all types of categories.',
+
+    designStrong: 'My experience:',
+    designList: [
+      'Nature photography, landscapes and people.',
+      'editing with Lightroom and Photoshop.',
+      'Designing and building websites.',
+    ],
+    designTools: 'Tools:',
+    designToolsList: ['Lightroom, Photoshop, Professional camera, Bootstrap 5, Material ui, css'],
+    developTitle: '',
+    developP: ' ',
+    developStrong: '',
+    developList: [''],
+    developTools: 'Tools:',
+    developToolsList: [''],
+  },
+  he: {
+    designTitle: 'מעצב אתרים, צלם וגרפיקאי',
+    designP:
+      'אני אוהב לעצב אתרים בצורה מושכת את העין, בסגנון נקי ומינימלי. נהנה לצלם ולערוך תמונות מכל סוגי קטגוריות. ',
+    designStrong: 'הניסיון  שלי:',
+    designList: [
+      'צלם טבע, נופים ואירועים.',
+      'ניסיון עשיר בעריכה בתוכנות לייטרום ופוטושופ.',
+      'עיצוב ובניית אתרים.',
+    ],
+
+    designTools: 'כלים:',
+    designToolsList: ['מצלמה מקצועית, לייטרום, פוטושופ,', ' css, Bootstrap 5, Material ui'],
+    developTitle: '',
+    developP: ' ',
+    developStrong: '',
+    developList: [''],
+    developTools: 'כלים:',
+    developToolsList: [''],
+  },
+};
+
 export default function mySkills() {
+  const [lang] = useContext(LanguageContext);
+  const {
+    designTitle,
+    designP,
+    designStrong,
+    designList,
+    designTools,
+    designToolsList,
+    developTitle,
+    developP,
+    developStrong,
+    developList,
+    developTools,
+    developToolsList,
+  } = Text[lang];
+
   return (
     <Container>
-      <Main></Main>
-      <Main></Main>
+      <Main lang={lang}>
+        <Title>
+          <BiPaint />
+          {designTitle}
+        </Title>
+        <Descrition>{designP}</Descrition>
+        <Descrition>
+          <strong>{designStrong}</strong>
+          <ul>
+            {designList.map((listText, i) => (
+              <li key={`k${i}`}>{listText}</li>
+            ))}
+          </ul>
+        </Descrition>
+        <Descrition>
+          <strong>{designTools}</strong>
+          <ul>
+            {designToolsList.map((listText, i) => (
+              <li key={`k${i}`}>{listText}</li>
+            ))}
+          </ul>
+        </Descrition>
+      </Main>
+      <Main lang={lang}>
+        <Title>
+          <FaLaptopCode />
+          {developTitle}
+        </Title>
+        <Descrition>{developP}</Descrition>
+        <Descrition>
+          <strong>{developStrong}</strong>
+          <ul>
+            {developList.map((listText, i) => (
+              <li key={`k${i}`}>{listText}</li>
+            ))}
+          </ul>
+        </Descrition>
+        <Descrition>
+          <strong>{developTools}</strong>
+          <ul>
+            {developToolsList.map((listText, i) => (
+              <li key={`k${i}`}>{listText}</li>
+            ))}
+          </ul>
+        </Descrition>
+      </Main>
     </Container>
   );
 }
